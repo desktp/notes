8c38bfab06f1ae1c70d11c999f496abbc96f2571
jacques mouette <j.mouette@gmail.com>
2018-08-08 16:14:20 -0300

    Add linkify package; Parse links in note text

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import linkifyHtml from 'linkifyjs/html';
 
 
 import './NoteCard.css';
 
 const NoteCard = ({ note: { title, text} }) => (
-  <Paper className="note-card-container" elevation={6}>
-    <Typography variant="title">{title}</Typography>
-    <Typography variant="body2">
-      {text}
-    </Typography>
+  <Paper className="note-card-container" elevation={12}>
+    <Typography variant="title" gutterBottom>{title}</Typography>
+    <Typography dangerouslySetInnerHTML={{ __html: linkifyHtml(text, { ignoreTags: ['script'] }) }}></Typography>
   </Paper>
-
 );
 
 export default NoteCard;

c858ab346447e4348656230f14bfb7ee5558a836
jacques mouette <j.mouette@gmail.com>
2018-08-08 16:27:25 -0300

    Reset InputCard state when submitting or blurring without typing anything

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -6,30 +6,37 @@ import Button from '@material-ui/core/Button';
 
 import './InputCard.css';
 
+const INITIAL_STATE = {
+  title: '',
+  text: '',
+  expanded: false,
+}
+
 class InputCard extends Component {
-  state = {
-    title: '',
-    text: '',
-    expanded: false,
-  }
+  state = INITIAL_STATE;
 
   expandCard = () => this.setState({ expanded: true });
 
   handleInputChange = (field, event) => this.setState({ [field]: event.target.value });
 
-  submitNote = () => this.props.addNote(this.state.title, this.state.text);
+  onBlur = () => !this.state.text && !this.state.title ? this.setState({ expanded: false }) : null;
+
+  submitNote = () => {
+    this.props.addNote(this.state.title, this.state.text);
+    this.setState(INITIAL_STATE);
+  }
 
   render() {
     const { expanded } = this.state;
 
     return (
       <Paper className="input-note-container">
-        <TextField onChange={e => this.handleInputChange('title', e)} value={this.state.title} placeholder="Add note..." onFocus={this.expandCard} />
+        <TextField onChange={e => this.handleInputChange('title', e)} onBlur={this.onBlur} value={this.state.title} placeholder="Add note..." onFocus={this.expandCard} />
         {
           expanded && 
             <React.Fragment>
-              <TextField onChange={e => this.handleInputChange('text', e)} value={this.state.field} multiline rows={3} />
-              <Button variant="contained" color="primary" onClick={() => this.submitNote()}>
+              <TextField onChange={e => this.handleInputChange('text', e)} onBlur={this.onBlur} value={this.state.field} multiline rows={3} />
+              <Button onClick={() => this.submitNote()}>
                 Submit
               </Button>
             </React.Fragment>
